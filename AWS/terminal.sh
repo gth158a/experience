a7afbdf0a9368f4d5d5379ad41fb74780401fa4d
Jaime Almeida <jaime.almeida@gatech.edu>
2017-06-13 14:57:09 -0700

    Added pushd and popd

---
+++ AWS/terminal.sh
@@ -0,0 +1,15 @@
+# Terminal
+
+pushd
+# It's like saying, "Save where I am, then go here."
+# takes your current directory and "pushes" it into a list for later,
+#then it changes to another directory.
+
+pushd, if you run it by itself with no arguments, 
+will switch between your current directory and the last one you pushed.
+
+#It's an easy way to switch between two directories.
+
+
+popd
+# takes the last directory you pushed and "pops" it off, taking you back there.
