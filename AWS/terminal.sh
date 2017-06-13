463c42146a928c182d79471f1f34046c82d65095
Jaime Almeida <jaime.almeida@gatech.edu>
2017-06-13 14:59:55 -0700

    Added text related commands

--- AWS/terminal.sh
+++ AWS/terminal.sh
@@ -15,6 +15,14 @@ popd
 # takes the last directory you pushed and "pops" it off, taking you back there.
 
 source filename
-
 # you need to source your dotfiles after making changes for them to take
 # effect (or log off and in again)
+
+# Dealing with Text
+# from http://mally.stanford.edu/~sr/computing/more-unix.html
+CTRL-d # delete next character
+CTRL-k # delete rest of line
+CTRL-a # go to start of line
+CTRL-e # go to end of line
+CTRL-b # go backwards without deleting
+CTRL-f # go forward without deleting
