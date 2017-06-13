197e62ecb5a6f4798e94d8fbc38b5d640cb3f336
Jaime Almeida <jaime.almeida@gatech.edu>
2017-06-13 14:58:10 -0700

    Added source

--- AWS/terminal.sh
+++ AWS/terminal.sh
@@ -5,7 +5,7 @@ pushd
 # takes your current directory and "pushes" it into a list for later,
 #then it changes to another directory.
 
-pushd, if you run it by itself with no arguments, 
+pushd, if you run it by itself with no arguments,
 will switch between your current directory and the last one you pushed.
 
 #It's an easy way to switch between two directories.
@@ -13,3 +13,8 @@ will switch between your current directory and the last one you pushed.
 
 popd
 # takes the last directory you pushed and "pops" it off, taking you back there.
+
+source filename
+
+# you need to source your dotfiles after making changes for them to take
+# effect (or log off and in again)
