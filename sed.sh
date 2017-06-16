ac0fc4478b89d6fe60e2760e5256fa43c9a54948
Jaime Almeida <jaime.almeida@gatech.edu>
2017-06-16 11:30:36 -0700

    Added shortcut to do TAB in terminal

--- sed.sh
+++ sed.sh
@@ -2,3 +2,4 @@
 
 cat pwd-only3 | sed 's/       / /g' | sed 's/,/ /g' > pwd-only3-def
 # to replace TAB you have to literally use a tab '\t' does not do the job
+# Precede it with Control + V, followed by Tab to suppress the usual expansion behavior.
