76d40c6f00e42b41355919e73eb597b565f4676b
Jaime Almeida <jaime.almeida@gatech.edu>
2017-06-30 00:19:27 -0700

    Added how to search process ID and kill process

---
+++ system.sh
@@ -0,0 +1,6 @@
+# to search for a particular process
+ps aux | grep server-1.properties
+#7564 ttys002    0:15.91 /System/Library/Frameworks/JavaVM.framework/Versions/1.8/Home/bin/java...
+
+# to kill the process searched above
+kill -9 7564
