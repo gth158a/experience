0958e4585fd7c03ad9a807374330ee6310e765f2
Jaime Almeida <jaime.almeida@gatech.edu>
2017-10-08 17:23:18 -0700

    Added information about profiles in aws cli

--- AWS/awscli.sh
+++ AWS/awscli.sh
@@ -20,3 +20,11 @@ aws s3 ls s3://sandbox-l2v --recursive
 
 aws s3 cp s3://sandbox-l2v/datasets/ml-1m/rmse/ /Users/jaimealmeida/repos/worklog/7-25 --recursive
 # to copy all files in folder from s3 to local folder
+
+aws configure --profile fastai
+# to create a profile named fastai with different credentials
+# you can see all the profiles configured with cat ~/.aws/credentials
+
+export AWS_PROFILE=user2
+# if profile user2 will be used for more than one command
+# otherwise you can append '--profile user2' to the command
