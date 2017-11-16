8b1b9cb3ff46f1cf8443e031d3bd17fecb5fe251
Jaime Almeida <jaime.almeida@gatech.edu>
2017-11-16 10:43:19 -0800

    Added redshift commands

---
+++ AWS/redshift.sql
@@ -0,0 +1,8 @@
+-- get date from timedate
+trunc()
+
+-- NLV or COALESCE evaluates expressions until a not NUll values is found
+-- http://docs.aws.amazon.com/redshift/latest/dg/r_NVL_function.html
+nlv(exp1, exp2, ...)
+
+-- SELECT INTO statement copies data from one table into a new table.
